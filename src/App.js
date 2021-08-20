50cdd14d6a706055dadd75daea08793c712dd662
Sandhra Rose <sandhrarosechackochan.b20me1136@mbcet.ac.in>
2021-08-20 21:11:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  let[num,setNum]=useState(0)
+
+  const increment=()=>{
+   setNum(num+1)
+  }
+  const decrement=()=>{
+   setNum(num-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="main__container"> 
+       <h1>{num}</h1>
+       <div>
+         <button onClick={increment}>ADD</button>
+         <button onClick={decrement}>SUB</button>
+       </div>
+     </div>
     </div>
   );
 }
 
 export default App;
+ 
